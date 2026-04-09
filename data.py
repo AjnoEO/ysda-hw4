f31718a9a68c34539373744b625ae110c120126c
Ajno <boyko.artiom@gmail.com>
2026-04-10 00:14:04 +0300

    Handle lack of HF token

--- data.py
+++ data.py
@@ -1,7 +1,11 @@
-import json
+import json, os
 
-with open("hf_token.txt", encoding="utf8") as f:
-    HF_TOKEN = f.read().strip()
+HF_TOKEN = None
+if "hf_token.txt" in os.listdir():
+    with open("hf_token.txt", encoding="utf8") as f:
+        HF_TOKEN = f.read().strip()
+else:
+    print("! HF_TOKEN is None")
 
 with open("metrics.json", encoding="utf8") as f:
     METRICS = json.load(f)
